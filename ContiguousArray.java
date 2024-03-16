95e77a72713dd37d9289166c6c7fb18cd1cc9e03
Rahul Goswami <75429646+visioncoding021@users.noreply.github.com>
2024-03-16 19:28:35 +0530

    Create ContiguousArray.java

---
+++ ContiguousArray.java
@@ -0,0 +1,30 @@
+
+// https://leetcode.com/problems/contiguous-array/description/?envType=daily-question&envId=2024-03-16
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                sum += -1;
+            } else {
+                sum += 1;
+            }
+            
+            if (sum == 0) {
+                max = Math.max(max, i + 1);
+            } else {
+                if (map.containsKey(sum)) {
+                    max = Math.max(max, i - map.get(sum));
+                } else {
+                    map.put(sum, i);
+                }
+            }
+        }
+        
+        return max;
+    }
+}
